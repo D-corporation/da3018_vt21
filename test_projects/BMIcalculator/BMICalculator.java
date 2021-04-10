a2549d2a8a5db6ab8bc725f90e85add6641ae8de
Daniel <danielfjberg@gmail.com>
2021-04-10 13:26:13 +0200

    BMIcalculator: added a main method

--- test_projects/BMIcalculator/BMICalculator.java
+++ test_projects/BMIcalculator/BMICalculator.java
@@ -6,12 +6,24 @@ public class BMICalculator {
         this.height = height;
     }
 
-    public void calculate() {
+    public void calculateBMI() {
         BMI = weight / (height * height);
     }
     
     public boolean isOverweight() {
         return (BMI > 25);
+    }
+
+    //this is a commment in java 
+    //below will our main method come 
+
+    public static void main(String[] args) {
+        BMICalculator calculator = new BMICalculator(60, 1.70);
+        double bmi = calculator.calculateBMI();
+        
+        //print out the bmi
+
+        system.out.println('Your BMI is ' + bmi + '.')
     }
 }
 
