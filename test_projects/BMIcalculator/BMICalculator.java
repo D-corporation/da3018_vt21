58d81278f47966827a094a22acbcfee27cc0e88a
Daniel <danielfjberg@gmail.com>
2021-04-10 13:12:48 +0200

    created bmi calculator

---
+++ test_projects/BMIcalculator/BMICalculator.java
@@ -0,0 +1,17 @@
+public class BMICalculator {
+    private double weight, height, BMI;
+    
+    public BMICalculator( double weight, double height) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public void calculate() {
+        BMI = weight / (height * height);
+    }
+    
+    public boolean isOverweight() {
+        return (BMI > 25)
+    }
+}
+
