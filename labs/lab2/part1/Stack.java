cf4d53567bdd39b2006393a8ebbf9889e6c3b5bf
Daniel <danielfjberg@gmail.com>
2021-04-13 13:51:03 +0200

    Lab2: some small improvements to the stack

--- labs/lab2/part1/Stack.java
+++ labs/lab2/part1/Stack.java
@@ -10,12 +10,12 @@ public class Stack {
     // N: stack overflow
     private int N = 1000;
 
-    private double[] array = new double[N-1];
+    private double[] array = new double[N];
 
     boolean is_empty() {
+
         // if top is -1 the stack is empty, returning true
         // else it is not empty, returning false
-
         if (top == -1) {
             return true;
         } else {
@@ -24,16 +24,23 @@ public class Stack {
     } 
 
     void push(double x) {
-        this.top += 1;
+        // top increases by 1
+        top += 1;
 
+        // inputs value x to the array at index top
         array[top] = x;
     }
 
     double pop() {
-
+        // saves the value that are goint to be "popped" temporarily for return value
         double popped_value = array[top];
+
+        // sets array value at index top to default value for double arrays which is 0.0 
         array[top] = 0.0;
+
+        // top decreases by 1
         top -= 1;
+
         return popped_value;
 
     }
